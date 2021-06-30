6513ebe85b855d9bf028719d81673f5e88042813
Ghulam Mustafa <mustafaji@gmail.com>
2021-06-30 21:25:09 +0200

    updated index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
