de298f327d569a31263406c67a9aa31899e83d25
Ghulam Mustafa <mustafaji@gmail.com>
2021-07-02 19:06:31 +0200

    updated App page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Login from "./Login";
 import Register from "./Register";
 import NoMatchPage from "./NoMatchPage";
@@ -6,20 +6,29 @@ import Dashboard from "./Dashboard";
 import { HashRouter } from "react-router-dom";
 import { Route, Switch } from "react-router";
 import NavBar from "./NavBar";
+import { UserContext } from "./UserContext";
 
 function App() {
+  let [user, setUser] = useState({
+    isLoggedIn: false,
+    currentUserId: null,
+    currentUserName: null,
+  });
+
   return (
-    <HashRouter>
-      <NavBar />
-      <div className="container-fluid">
-        <Switch>
-          <Route path="/" exact={true} component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="*" component={NoMatchPage} />
-        </Switch>
-      </div>
-    </HashRouter>
+    <UserContext.Provider value={{ user, setUser }}>
+      <HashRouter>
+        <NavBar />
+        <div className="container-fluid">
+          <Switch>
+            <Route path="/" exact={true} component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="*" component={NoMatchPage} />
+          </Switch>
+        </div>
+      </HashRouter>
+    </UserContext.Provider>
   );
 }
 
