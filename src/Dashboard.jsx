ea7e5504a37a2de35b7e958acf5b4a38f2c323c0
Ghulam Mustafa <mustafaji@gmail.com>
2021-07-02 19:09:34 +0200

    updated Dashboard

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { UserContext } from "./UserContext";
 
 function Dashboard() {
+    //executes only once - on initial render =  componentDidMount
+    useEffect(() => {
+        document.title = "Dashboard - eCommerce";
+    }, []);
+
+    //get context
+    let userContext = useContext(UserContext);
+    console.log(userContext);
+
     return (
         <div>
             <h1>Dashboard</h1>
